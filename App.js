29d574381fcf7510fbcafbe74fc533409118a383
Shashank <shashankavhad03@gmail.com>
2022-02-02 11:12:14 +0530

    Changes in UnitGoals and TargetScreen added

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import IntroductionScreen from "./src/screens/authscreens/IntroductionScreen";
 import LaunchScreen from "./src/screens/authscreens/LaunchScreen";
 import WorkTabScreen from "./src/screens/WorkTab/WorkTabScreen";
 import UnitGoalsScreen from "./src/screens/WorkTab/UnitGoalsScreen";
+import TargetScreen from "./src/screens/WorkTab/TargetScreen";
 
 const navigator = createStackNavigator(
   {
@@ -13,6 +14,7 @@ const navigator = createStackNavigator(
     introduction: IntroductionScreen,
     workTab: WorkTabScreen,
     unitGoals: UnitGoalsScreen,
+    targets: TargetScreen,
   },
   {
     initialRouteName: "unitGoals",
